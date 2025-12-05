0e0d198e63d812cf4dbdd9a2d8496aeba045c9a5
Abhinav <abhinavmukherjee.lko@gmail.com>
2025-12-06 02:55:48 +0530

    day1

--- Daily-Python-Practice/Day 1.py
+++ Daily-Python-Practice/Day 1.py
@@ -1,4 +1,14 @@
-#Day1
-a = 2
-b = 2
-print(a + b)
+#Create a simple calculator
+a = int(input("enter the valur of a"))
+b = int(input("enter the value of b"))
+c = int(input("Select option for performance\n1 ,2 ,3 ,4"))
+if (c == 1):
+    print("Solution =", a +b)
+elif(c == 2):
+    print("Solution =", a - b)
+elif(c == 3):
+    print("Solution =", a *b)
+elif(c == 4):
+    print("Solution =", a /b)
+else:
+    print("wrong selection")
