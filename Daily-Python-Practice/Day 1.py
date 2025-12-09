3956b965e89d3f5416259093bfdb49f44dddde08
Abhinav <abhinavmukherjee.lko@gmail.com>
2025-12-10 02:48:44 +0530

    new work

--- Daily-Python-Practice/Day 1.py
+++ Daily-Python-Practice/Day 1.py
@@ -15,4 +15,52 @@ else:
 #length of a name
 '''names = "abhinav"
 print(len(names))'''
-
+#program for determinig whether a person can drive or not
+'''age = int(input("Enter Your Age ="))
+if (age >= 18):
+    print("you can drive")
+else:
+    print("you cannot drive")'''
+#Greeting Program to User Using time library
+'''import time
+timestamp = time.strftime('%H,%M,%S')
+print(timestamp)
+if (20>int(time.strftime('%H'))>12):
+    print("Good Evening")
+elif(5<int(time.strftime('%H'))<12):
+    print("Good Morning")
+else:
+    print("Good Night")'''
+#Switch-Case Statement
+#Day of the Week
+'''day = int(input("enter the day :"))
+match day:
+    case (1):
+        print("Monday")
+    case (2):
+        print("tuesday")
+    case (3):
+        print("Wednesday")
+    case (4):
+        print("Thursday")
+    case (5):
+        print("Friday")
+    case (6):
+        print("Saturday")
+    case (7):
+        print("Sunday")'''
+# For loops
+'''colours = ["red","green","blue","yellow"]
+for i in colours:
+    print(i)
+    for j in i:
+        print(j)'''
+'''Arr = [2,3,52,1,5,4,65,75,33,234,88]
+for i in Arr:
+    print(i)'''
+#range
+'''for i in range(20):
+    print(i)'''
+'''for i in range(0,31,2):
+    print(i)'''
+#while loop
