3a7919cd9742cb4b2a8c98111dc596290112b177
Abhinav <abhinavmukherjee.lko@gmail.com>
2025-12-11 20:13:13 +0530

    new work

--- Daily-Python-Practice/Day2.py
+++ Daily-Python-Practice/Day2.py
@@ -1,1 +1,21 @@
-#addDay2
+#Break and Continue
+#Searching the no. in array using break(linear search)
+'''arr = [2,3,6,7,89,4,7,56,448,2]
+no = int(input("enter the no. you to search"))
+for i in arr:
+    if(no == i):
+        print("found")
+        break
+else:
+    print("not found")'''
+#Printing odd no. till 100 using Continue
+'''for i in range(100):
+    if (i%2 == 0):
+        continue
+    print(i)'''
+#Printing even no. till 100
+'''for i in range(102):
+    if (i%2 == 1):
+        continue
+    print(i)'''
+
